5ed198fb3567a930ab2198f1705b4e7cccff12a6
Simen Li <simenkid@gmail.com>
2015-12-31 14:22:29 +0800

    going

--- mqtt-node.lua
+++ mqtt-node.lua
@@ -37,7 +37,39 @@ function MqttNode:new(info)
 
     --info.mc = mqtt.Client("clientId", 120, "user", "password")
     info.mc = 'fake'
-    info.so = {}
+    info.so = { -- Default Objects
+        [1] = {         -- LWM2M Object: LWM2M Server Object
+            [0] = nil,              -- short server id
+            [1] = info.lifetime,    -- lifetime
+            [2] = 1,                -- default pmin in seconds
+            [3] = 60,               -- default pmax on seconds
+            [8] = {                 -- registration update trigger
+                exec: function ()
+                end
+            }
+        },
+        [3] = {         -- LWM2M Object: Device
+            [0] = 'freebird',   -- manuf
+            [1] = 'mqtt-node',  -- model
+            [4] = {             -- reboot
+                exec = function ()
+                end
+            },
+            [5] = {             -- factory reset
+                exec = function ()
+                end
+            },
+            [6] = 0,            -- available power sources
+            [7] = 5000,         -- power source voltage
+            [17] = 'generic',   -- device type
+            [18] = 'v0.0.1',    -- hardware version
+            [19] = 'v0.0.1',    -- software version
+        },
+        [4] = {         -- LWM2M Object: Connectivity Monitoring
+            [4] = info.ip,      -- ip address
+            [5] = nil           -- router ip address
+        }
+    }
 
     info._pubics = {
         register = 'register/' .. info.clientId,
@@ -56,7 +88,9 @@ function MqttNode:new(info)
         ping = 'ping/response/' .. info.clientId,
         request = 'request/' .. info.clientId,
         announce = 'announce'
-    };
+    }
+
+    info.repAttrs = {}
 
     return setmetatable(info, self)
 end
@@ -183,13 +217,18 @@ function MqttNode:dump()
 end
 
 
--- -- function MqttNode:connect(url, opts)
--- --     -- opts = opts or {}
--- --     -- local port = opts
--- --     self.mc.connect(host, port, secure, function ()
+function MqttNode:connect(url, opts)
+    -- opts = opts or {}
+    -- local port = opts
+    -- [TODO]
 
--- --     end)
--- -- end
+    -- (1) remove listeners
+
+    -- (2) re-attch listeners
+    self.mc.connect(host, port, secure, function (client)
+
+    end)
+end
 
 -- -- function MqttNode:encrypt(msg)
 -- --     return msg
@@ -206,25 +245,74 @@ end
 
 -- -- end
 
--- -- function MqttNode:pubRegister()
+function MqttNode:objectList()
+    local objList = {}
 
--- -- end
+    for oid, obj in pairs(self.so) do
+        for iid, inst in pairs(obj) do
+            table.insert(objList, {
+                oid = oid,
+                iid = iid
+            })
+        end
+    end
 
--- -- function MqttNode:pubDeregister()
+    return objList
+end
 
--- -- end
+function MqttNode:pubRegister(callback)
+    local regPubChannel = self._pubics.register
+    local regData = {
+            lifetime = self.lifetime,
+            objList = self.objectList(),
+            ip = self.ip,
+            mac = self.mac,
+            version = self.version,
+         }
 
--- -- function MqttNode:pubNotify()
+    self:publish(regPubChannel, regData, callback)
+end
 
--- -- end
+function MqttNode:pubDeregister(callback)
+    local deregPubChannel = self._pubics.deregister
+    self:publish(deregPubChannel, { data = nil }, callback)
+end
 
--- -- function MqttNode:pingServer()
+function MqttNode:pubNotify(data, callback)
+    local notifyPubChannel = self._pubics.notify
+    self:publish(notifyPubChannel, data, callback)
+end
 
--- -- end
+function MqttNode:pingServer(callback)
+    local pingPubChannel = self._pubics.ping
+    self:publish(pingPubChannel, { data = nil }, callback)
+end
 
--- -- function MqttNode:publish()
+function MqttNode:pubUpdate(devAttrs, callback)
+    local updatePubChannel = self._pubics.update
+    assert(type(devAttrs) == "table", "devAttrs should be a table.")
+    assert(devAttrs.mac == nil, "mac cannot be changed at runtime.")
+    self:publish(updatePubChannel, devAttrs, callback)
+end
 
--- -- end
+function MqttNode:pubResponse(rsp, callback)
+    local rspPubChannel = self._pubics.response
+    self:publish(rspPubChannel, rsp, callback)
+end
+
+function MqttNode:publish(topic, message, qos, retain, callback)
+    -- [TODO] default options?
+    m:publish(topic, message, qos, retain, function (client)
+        self.emit('published', {
+            topic = topic,
+            message = message,
+            options = options
+        })
+
+        if (callback ~= nil) then
+            callback()
+    end)
+end
 
 -- -- function MqttNode:subscribe()
 
@@ -266,6 +354,104 @@ end
 -- [[ *********************************************
 --    **  Protected Methods                      **
 --   ********************************************** ]]
+function MqttNode:_target(oid, iid, rid)
+    local tgtype
+    local target
+
+    if (oid ~= nil) then
+        tgtype = 'object'
+        target = self.so[oid]
+    end
+
+    if (iid ~= nil) then
+        tgtype = 'instance'
+        target = self.so[oid]
+        if (target ~= nil) then  target = self.so[oid][iid] end
+    end
+
+    if (iid ~= nil) then
+        tgtype = 'resource'
+        target = self.so[oid]
+        if (target ~= nil) then target = self.so[oid][iid] end
+        if (target ~= nil) then target = self.so[oid][iid][rid] end
+    end
+
+    if (target == nil) then
+        target = '__notfound'
+    end
+
+    return tgtype, target
+end
+
+function MqttNode:getAttrs(...)
+    -- args: oid, iid, rid
+    local oid = arg[1]
+    local iid = nil
+    local rid = nil
+    local tgtype, target
+    local attr
+
+    if (#arg == 3) then
+        iid = arg[2]
+        rid = arg[3]
+    elseif (#arg == 2) then
+        iid = arg[2]
+    end
+
+    tgtype, target = self:_target(oid, iid, rid)
+    if (target == nil) then
+        return '__notfound'
+    end
+
+    if (tgtype == 'object') then
+        attr = self.repAttrs[oid]
+    elseif (tgtype == 'instance') then
+        attr = self.repAttrs[oid]
+        if (attr ~= nil) then attr = attr[iid] end
+    elseif (tgtype == 'resource') then
+        attr = self.repAttrs[oid]
+        if (attr ~= nil) then attr = attr[iid] end
+        if (attr ~= nil) then attr = attr[rid] end
+    end
+
+    return attr
+end
+
+function MqttNode:setAttrs(...)
+    -- args: oid, iid, rid, attrs
+    local oid = arg[1]
+    local iid
+    local rid
+    local attrs
+    local tgtype
+
+    self.repAttrs[oid] = self.repAttrs[oid] or {}
+
+    if (#arg == 4) then
+        iid = arg[2]
+        rid = arg[3]
+        attrs = arg[4]
+    elseif (#arg == 3) then
+        iid = arg[2]
+        attrs = arg[3]
+    elseif (#arg == 2) then
+        attrs = arg[2]
+    end
+
+    tgtype = self:_target(oid, iid, rid)
+
+    if (tgtype == 'object') then
+        self.repAttrs[oid] = attrs
+    elseif (tgtype == 'instance') then
+        self.repAttrs[oid][iid] = self.repAttrs[oid][iid] or {}
+        self.repAttrs[oid][iid] = attrs
+    elseif (tgtype == 'resource') then
+        self.repAttrs[oid][iid] = self.repAttrs[oid][iid] or {}
+        self.repAttrs[oid][iid][rid] = self.repAttrs[oid][iid][rid] or {}
+        self.repAttrs[oid][iid][rid] = attrs
+    end
+end
+
 function MqttNode:_initResrc(oid, iid, rid, value)
     local iobj = self.so[oid][iid]
     local isCb = false
@@ -326,21 +512,70 @@ return MqttNode
 -- APIs
 
 -- new MqttNode
--- setDevAttrs
--- initResrc
--- readResrc
--- writeResrc
-
+-- V setDevAttrs
+-- V initResrc
+-- V readResrc
+-- V writeResrc
+-- V dump
 -- connect
 -- close
 
--- pubRegister
--- pubDeregister
--- pubNotify
--- pingServer
+-- V pubRegister
+-- V pubDeregister
+-- V pubNotify
+-- V pingServer
 
--- publish
+-- V publish
 -- subscribe
 -- unsubscribe
 -- --]]
 
+function msgHandler(conn, topic, data)  -- conn is client
+    local message = self:decrypt(message)
+    local evt
+    self.emit('message', topic, message);
+
+    local jsonMsg = cjson.decode(message)
+    local path = slashPath(topic)
+
+    if (path == self._subics.register) then
+        evt = 'reg_rsp'
+    elseif (path == self._subics.deregister) then
+        evt = 'dereg_rsp'
+    elseif (path == self._subics.notify) then
+        evt = 'notify_rsp'
+    elseif (path == self._subics.update) then
+        evt = 'update_rsp'
+    elseif (path == self._subics.ping) then
+        evt = 'ping_rsp'
+    elseif (path == self._subics.request) then
+        evt = 'request'
+    elseif (path == self._subics.announce) then
+        evt = 'announce'
+    end
+
+    if (evt ~= nil) then
+        self.emit(evt, jsonMsg)
+    end
+
+    if (evt == 'request') then
+        self.emit('_request', jsonMsg)
+    end
+
+
+end
+
+function slashPath(path)
+    local head = 1
+    local tail = #path
+    path = path:gsub("%.", "/")
+    if (path:sub(1, 1) == '/') then
+        head = 2
+    end
+
+    if (path:sub(#path, #path) == '/') then
+        tail = tail - 1
+    end
+
+    return path:sub(head, tail);
+end
--- test2.lua
+++ test2.lua
@@ -1,2 +1,18 @@
-local x = '1000'
-print(tonumber(x))
+function slashPath(path)
+    local head = 1
+    local tail = #path
+    path = path:gsub("%.", "/")
+    if (path:sub(1, 1) == '/') then
+        head = 2
+    end
+
+    if (path:sub(#path, #path) == '/') then
+        tail = tail - 1
+    end
+
+    return path:sub(head, tail);
+end
+
+
+local s = slashPath('.a.b.c.')
+print(s)
